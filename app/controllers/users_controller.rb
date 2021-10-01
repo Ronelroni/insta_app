68a019f1af80f874002ced42cf806df5dcfd8d83
KOUAGOU <ronelroni01@gmail.com>
2021-10-01 18:14:04 +0100

    Mes Corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-    skip_before_action :login_required, only: [:new, :create]
+    skip_before_action :login_required, only: [:new, :create, :edit]
     def new
         @user = User.new
+    end
+    def index
+        @users = User.all
+      end
+    def edit
+        @user = User.find(params[:id])
     end
     def create
         @user = User.new(user_params)
@@ -17,16 +23,22 @@ class UsersController < ApplicationController
 
      # PATCH/PUT /users/1 or /users/1.json
     def update
+        @user = User.find(params[:id])
         respond_to do |format|
-            if @user.update(post_params)
-              format.html { redirect_to @user, @user.name, @user.email }
-            else
-              format.html { render :edit, status: :unprocessable_entity }
-            end
+            if @user.update(user_params)
+                format.html { redirect_to @user, notice: "User was successfully updated." }
+                format.json { render :show, status: :ok, location: @user }
+              else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+              end
           end
     end
     
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
     def user_params
       params.require(:user).permit(:name, :email, :password, :image, :image_cache,
                                      :password_confirmation)
